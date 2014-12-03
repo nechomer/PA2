8e0fda814085d3d3fd4dc67767bc725835a86f21
liorberc <liorberc1@gmail.com>
2014-12-03 21:34:32 +0200

    Uminus

--- our_files/src/ic/parser/ProgramParserSym.java
+++ our_files/src/ic/parser/ProgramParserSym.java
@@ -8,55 +8,56 @@ package ic.parser;
 /** CUP generated class containing symbol constants. */
 public class ProgramParserSym {
   /* terminals */
-  public static final int FALSE_LITERAL = 46;
-  public static final int STRING_LITERAL = 47;
-  public static final int GTEQ = 32;
+  public static final int FALSE_LITERAL = 47;
+  public static final int STRING_LITERAL = 48;
+  public static final int GTEQ = 33;
   public static final int LBRACKET = 18;
-  public static final int MULT = 25;
-  public static final int LTEQ = 30;
+  public static final int MULT = 26;
+  public static final int LTEQ = 31;
+  public static final int UMINUS = 25;
   public static final int INTEGER = 6;
-  public static final int SEMI = 40;
+  public static final int SEMI = 41;
   public static final int LPAREN = 20;
   public static final int CONTINUE = 14;
-  public static final int TRUE_LITERAL = 45;
+  public static final int TRUE_LITERAL = 46;
   public static final int MINUS = 23;
   public static final int STATIC = 4;
   public static final int RPAREN = 21;
-  public static final int AND = 35;
+  public static final int AND = 36;
   public static final int NOT = 24;
-  public static final int LT = 29;
-  public static final int OR = 36;
+  public static final int LT = 30;
+  public static final int OR = 37;
   public static final int CLASS = 2;
-  public static final int DIV = 26;
-  public static final int PLUS = 28;
-  public static final int NULL_LITERAL = 48;
+  public static final int DIV = 27;
+  public static final int PLUS = 29;
+  public static final int NULL_LITERAL = 49;
   public static final int IF = 10;
   public static final int THIS = 15;
   public static final int DOT = 22;
   public static final int EOF = 0;
   public static final int BOOLEAN = 7;
   public static final int RETURN = 9;
-  public static final int MODULU = 27;
+  public static final int MODULU = 28;
   public static final int RBRACKET = 19;
   public static final int NEW = 16;
   public static final int error = 1;
-  public static final int COMA = 41;
-  public static final int EQEQ = 33;
-  public static final int NEQ = 34;
+  public static final int COMA = 42;
+  public static final int EQEQ = 34;
+  public static final int NEQ = 35;
   public static final int BREAK = 13;
   public static final int VOID = 5;
-  public static final int EQ = 37;
-  public static final int LBRACE = 38;
+  public static final int EQ = 38;
+  public static final int LBRACE = 39;
   public static final int ELSE = 11;
   public static final int WHILE = 12;
-  public static final int CLASS_ID = 43;
-  public static final int RBRACE = 39;
+  public static final int CLASS_ID = 44;
+  public static final int RBRACE = 40;
   public static final int EXTENDS = 3;
   public static final int STRING = 8;
-  public static final int GT = 31;
+  public static final int GT = 32;
   public static final int LENGTH = 17;
-  public static final int INTEGER_LITERAL = 44;
-  public static final int IDENTIFIER = 42;
+  public static final int INTEGER_LITERAL = 45;
+  public static final int IDENTIFIER = 43;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -83,6 +84,7 @@ public class ProgramParserSym {
   "DOT",
   "MINUS",
   "NOT",
+  "UMINUS",
   "MULT",
   "DIV",
   "MODULU",
